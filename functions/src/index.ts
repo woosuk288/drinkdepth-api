fe4ffc78e44a27b8434362a144d5df7ed966ed5d
matrix <woosuk288@gmail.com>
2022-08-10 20:15:12 +0900

    BLD: serviceAccount.json 타입 에러 처리 및 tsconfig 수정

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,15 @@
-import * as functions from "firebase-functions";
+import functions from "firebase-functions";
 
-import * as admin from "firebase-admin";
-import * as express from "express";
+import admin, {ServiceAccount} from "firebase-admin";
+import serviceAccount from "../../../serviceAccount.json";
+import express from "express";
 
 import kakaoRouter from "./kakaoRouter";
-
-admin.initializeApp(); // 어드민 초기화. 클라우드 함수, 호스팅만 사용할 경우 따로 설정파일을 넘겨주지 않아도 됨
+// 어드민 초기화. 클라우드 함수, 호스팅만 사용할 경우 따로 설정파일을 넘겨주지 않아도 됨
+// 커스텀 인증(kakao 처리용) 사용하므로 serviceAccount 필요
+admin.initializeApp({
+  credential: admin.credential.cert(<ServiceAccount>serviceAccount),
+});
 const app = express();
 
 app.use(express.json()); // body-parser 설정
